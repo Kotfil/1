874a2419f6d6d1bdb905bee6c7c78d6bc91b6255
Filip <39651273+Kotfil@users.noreply.github.com>
2019-05-06 13:16:44 +0300

    part1

--- if/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ if/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -40,15 +40,17 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Узнать";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(61, 38);
+            this.label1.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(201, 183);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(42, 17);
+            this.label1.Size = new System.Drawing.Size(321, 68);
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
@@ -60,7 +62,6 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
--- if/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ if/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -10,11 +10,21 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
+   
     public partial class Form1 : Form
     {
+        int number = 10;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if(number == 10)
+            {
+
+            }
         }
     }
 }
