2ec9488c17f92e3ebde90a0b9b43f7797604ac5e
Filip <39651273+Kotfil@users.noreply.github.com>
2019-05-08 13:24:47 +0300

    next

---
+++ method/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void Button1_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(@"C:\Users\Fil\Desktop\1.cmd"))
+            {
+                MessageBox.Show("Файл есть', 'Есть ли файл", MessageBoxIcon.Asterisk);
+            }
+
+        }
+        string Method()
+        {
+            return Location.ToString();
+        }
+
+
+
+
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
