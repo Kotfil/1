5db0f755af4f9cc1c966fe2bd72d34e170f676c2
Filip <39651273+Kotfil@users.noreply.github.com>
2019-05-14 18:01:57 +0300

    “new”

---
+++ videosharp/EngCard/EngCard/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace EngCard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1_russian = new System.Windows.Forms.Label();
+            this.label2_english = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1_russian
+            // 
+            this.label1_russian.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1_russian.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1_russian.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1_russian.Location = new System.Drawing.Point(127, 32);
+            this.label1_russian.Name = "label1_russian";
+            this.label1_russian.Size = new System.Drawing.Size(272, 42);
+            this.label1_russian.TabIndex = 0;
+            this.label1_russian.Text = "label1";
+            this.label1_russian.UseMnemonic = false;
+            this.label1_russian.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2_english
+            // 
+            this.label2_english.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2_english.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2_english.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label2_english.Location = new System.Drawing.Point(127, 362);
+            this.label2_english.Name = "label2_english";
+            this.label2_english.Size = new System.Drawing.Size(272, 33);
+            this.label2_english.TabIndex = 1;
+            this.label2_english.Text = "label2";
+            this.label2_english.UseMnemonic = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(405, 206);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(46, 206);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(148, 97);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(242, 242);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 416);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2_english);
+            this.Controls.Add(this.label1_russian);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1_russian;
+        private System.Windows.Forms.Label label2_english;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
